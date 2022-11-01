8b054da8ec65d1a34f07712456c9f0bc0d38f9d2
Charlie <khwc1951@naver.com>
2022-11-01 15:23:09 +0900

    git commit -m "commit done?"

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,5 +6,10 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+
+    }
+    public  static void main(string args[]){
+        printf("hello world");
+
     }
 }
